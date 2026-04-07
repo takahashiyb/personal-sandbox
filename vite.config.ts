f1b54f8019b9d45dbcad61ea4bdec34c800e52f9
takahashiyb <takahashiyb89@gmail.com>
2026-04-07 17:37:51 +0800

    Set up sass

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,21 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/personal-sandbox/', 
+  base: '/personal-sandbox/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @use "@/assets/styles/variables.scss" as v;
+        @use "@/assets/styles/functions.scss" as f;
+        `,
+      },
+    },
+  },
 })
