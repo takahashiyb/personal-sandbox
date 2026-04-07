6644ff63d06bf13d69c6f6bd265918ce4836b62d
takahashiyb <takahashiyb89@gmail.com>
2026-04-08 01:44:40 +0800

    Set up header

---
+++ src/sections/HeaderSection.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import ThemeButton from '@/components/ThemeButton.vue'
+import { useThemeStore } from '@/stores/theme'
+
+const theme = useThemeStore()
+
+function switchTheme() {
+  theme.dark = !theme.dark
+}
+</script>
+<template>
+  <header class="header">
+    <ThemeButton :buttonPress="switchTheme" />
+  </header>
+</template>
+<style scoped lang="scss">
+.header {
+  display: flex;
+  flex-direction: row-reverse;
+}
+</style>
