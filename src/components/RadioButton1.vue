80650ad424bbb1ae053a905f6d932fe6dab367ff
takahashiyb <takahashiyb89@gmail.com>
2026-04-07 22:28:47 +0800

    Create Radio button 1 component

---
+++ src/components/RadioButton1.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const model = defineModel()
+
+const props = defineProps<{
+  name: string
+  value: string | number | null
+}>()
+</script>
+<template>
+  <!--
+  <RadioButton1 v-model="required" name="required" value="required">
+    <p>make design here</p>
+  </RadioButton1>
+  -->
+  <label>
+    <input
+      class="radio-element sr-only"
+      type="radio"
+      v-model="model"
+      :name="props.name"
+      :data-function="`radio-${props.name}`"
+      :value="props.value"
+    />
+    <div class="radio-display">
+      <slot></slot>
+    </div>
+  </label>
+</template>
+<style scoped lang="scss">
+// :has(input:checked) --> additional selector to style selected radio button
+</style>
