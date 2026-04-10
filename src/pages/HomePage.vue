4e9818ca4eefff884b46f977af083258507d0515
takahashiyb <takahashiyb89@gmail.com>
2026-04-11 03:02:14 +0800

    Setup Homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts"></script>
 <template>
-  <div><p>a</p></div>
+  <div class="content"></div>
 </template>
 <style scoped lang="scss">
 div {
   background-color: var(--secondary-bg-color);
-  height: 100px;
 }
 </style>
