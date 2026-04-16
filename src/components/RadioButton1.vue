bbed79503866ec45ffecbc2adab7582a82f8b815
takahashiyb <takahashiyb89@gmail.com>
2026-04-17 02:28:05 +0800

    Refactor component so the display is outside of the label

--- src/components/RadioButton1.vue
+++ src/components/RadioButton1.vue
@@ -1,31 +1,45 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 const model = defineModel()
 
 const props = defineProps<{
   name: string
   value: string | number | null
 }>()
+
+const click = ref<HTMLInputElement | null>(null)
+
+function clickButton() {
+  console.log('hello')
+  click.value!.click()
+}
 </script>
+<!--
+<RadioButton1 v-model="required" name="required" value="required">
+  <p>make design here</p>
+</RadioButton1>
+-->
 <template>
-  <!--
-  <RadioButton1 v-model="required" name="required" value="required">
-    <p>make design here</p>
-  </RadioButton1>
-  -->
   <label>
     <input
-      class="radio-element sr-only"
+      class="sr-only"
       type="radio"
       v-model="model"
       :name="props.name"
       :data-function="`radio-${props.name}`"
       :value="props.value"
+      ref="click"
     />
-    <div class="radio-display">
-      <slot></slot>
-    </div>
   </label>
+  <div class="radio-display" :data-function="`radio-display-${props.name}`" @click="clickButton">
+    <slot></slot>
+  </div>
 </template>
 <style scoped lang="scss">
-// :has(input:checked) --> additional selector to style selected radio button
+// .class-of-container:has(label input:checked) .class-of-display --> additional selector to style selected radio button
+
+.radio-display {
+  cursor: pointer;
+}
 </style>
