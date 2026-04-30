405e50a0ee17c408bb323342ae4a2e09232bb019
takahashiyb <takahashiyb89@gmail.com>
2026-05-01 04:20:55 +0800

    Refactor some styling

--- src/sections/HeaderSection.vue
+++ src/sections/HeaderSection.vue
@@ -8,16 +8,11 @@ import ThemeButton from '@/components/ThemeButton.vue'
 </template>
 <style scoped lang="scss">
 header {
-  height: var(--header-height);
-
-  padding-top: v.$spacing-0200;
-  padding-bottom: v.$spacing-0200;
-  padding-inline: v.$spacing-0200;
+  padding: v.$spacing-0200;
 
   display: flex;
   flex-direction: row-reverse;
-
-  justify-self: center;
+  gap: v.$spacing-0300;
 
   position: fixed;
   top: 0;
